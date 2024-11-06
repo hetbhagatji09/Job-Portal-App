2022fb91bf07a108be343165be5ca2b6f5b609a2
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-06 12:31:04 +0530

    Move jobController to controller package

--- src/main/java/com/example/Demo1/Controller/JobController.java
+++ src/main/java/com/example/Demo1/Controller/JobController.java
@@ -1,4 +1,4 @@
-package com.example.Demo1;
+package com.example.Demo1.Controller;
 
 import com.example.Demo1.Services.JobService;
 import com.example.Demo1.model.JobPost;
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
